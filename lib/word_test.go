27d5ee10d4f3970070037730ab6d5eedd97ed26f
yagi <hiro.yagi0245@gmail.com>
2017-06-12 02:32:35 +0900

    else test

--- lib/word_test.go
+++ lib/word_test.go
@@ -5,18 +5,27 @@ import (
 	"testing"
 )
 
-func TestIf(t *testing.T) {
+func TestIfElseThen(t *testing.T) {
 	i := NewInterpreter()
 	var tests = []struct {
 		input string
 		want  string
 	}{
-		{"0 hello", "[1 2]"},
-		{"1 hello", "[2]"},
+		{"0 hello .s", "[1 2]"},
+		{"1 hello .s", "[2]"},
+		{"0 foo .s", "[1 2 2]"},
+		{"1 foo .s", "[2]"},
+		{"0 hoge .s", "[1 2]"},
+		{"1 hoge .s", "[3 4]"},
 	}
 
-	i.SetString(": hello if 1 then 2 ;")
-	fmt.Println(": hello if 1 then 2 ;")
+	i.SetString(`
+: hello if 1 then 2 ;
+: foo if 0 hello then 1 hello ;
+: hoge if 1 2 else 3 4 then ;
+see hello
+see foo
+see hoge`)
 	if err := i.Run(); err != nil {
 		t.Error(err.Error())
 	}
@@ -24,10 +33,11 @@ func TestIf(t *testing.T) {
 	for _, test := range tests {
 		i.DS.Clear()
 		i.SetString(test.input)
+		fmt.Println(test.input)
 		if err := i.Run(); err != nil {
 			t.Error(err.Error())
 		}
-
+		// string comparison
 		if i.DS.String() != test.want {
 			t.Errorf("%q => %v", test.input, i.DS.data)
 		}
