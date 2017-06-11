84179a13ee17209fdc3dbba045e85ab8084d7a63
yagi <hiro.yagi0245@gmail.com>
2017-06-11 22:12:58 +0900

    add custom error

--- lib/error.go
+++ lib/error.go
@@ -9,3 +9,15 @@ func UndefinedError(text string) error {
 var UnderFlowError = fmt.Errorf("Stack underflow")
 
 var EOFError = fmt.Errorf("Unexpected EOF")
+
+var NoLastWordError = fmt.Errorf("No last word")
+
+var QuitError = fmt.Errorf("Bye!")
+
+var UnstructuredError = fmt.Errorf("unstructured control flow")
+
+func CompileOnlyError(text string) error {
+	return fmt.Errorf("compile only word %q", text)
+}
+
+var TypeError = fmt.Errorf("type mismatch")
